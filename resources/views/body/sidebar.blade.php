69d33aca741c9eff4ae7844c438a098169cfae2a
alireza375 <alirezaraju8@gmail.com>
2023-12-29 12:33:49 +0600

    first and completed commit

---
+++ resources/views/body/sidebar.blade.php
@@ -0,0 +1,139 @@
+<div class="left-side-menu">
+
+    <div class="h-100" data-simplebar>
+
+        <!-- User box -->
+
+        <!--- Sidemenu -->
+        <div id="sidebar-menu">
+            <ul id="side-menu">
+                <li class="menu-title">Navigation</li>
+                <li>
+                    <a href="{{ url('/dashboard') }}">
+                        <i class="mdi mdi-view-dashboard-outline"></i>
+                        <span> Dashboards </span>
+                    </a>
+                </li>
+
+                <li class="menu-title mt-2">Apps</li>
+
+                <li>
+                    <a href="#sidebarEcommerce" data-bs-toggle="collapse">
+                        <i class="mdi mdi-cart-outline"></i>
+                        <span> Employee Manage </span>
+                        <span class="menu-arrow"></span>
+                    </a>
+                    <div class="collapse" id="sidebarEcommerce">
+                        <ul class="nav-second-level">
+                            <li>
+                                <a href="{{ route('all.employee') }}">All Employee</a>
+                            </li>
+                            <li>
+                                <a href="{{ route('add.employee') }}">Add Employee </a>
+                            </li>
+                        </ul>
+                    </div>
+                </li>
+
+                <li>
+                    <a href="#sidebarCrm" data-bs-toggle="collapse">
+                        <i class="mdi mdi-account-multiple-outline"></i>
+                        <span> Customer Manage </span>
+                        <span class="menu-arrow"></span>
+                    </a>
+                    <div class="collapse" id="sidebarCrm">
+                        <ul class="nav-second-level">
+                            <li>
+                                <a href="{{ route('all.customer') }}">All Customer</a>
+                            </li>
+                            <li>
+                                <a href="{{ route('add.customer') }}">Add Customer</a>
+                            </li>
+                        </ul>
+                    </div>
+                </li>
+
+                <li>
+                    <a href="#sidebarEmail" data-bs-toggle="collapse">
+                        <i class="mdi mdi-email-multiple-outline"></i>
+                        <span> Supplier Manage </span>
+                        <span class="menu-arrow"></span>
+                    </a>
+                    <div class="collapse" id="sidebarEmail">
+                        <ul class="nav-second-level">
+                            <li>
+                                <a href="{{ route('all.supplier') }}">All Supplier</a>
+                            </li>
+                            <li>
+                                <a href="{{ route('add.supplier') }}">Add Supplier</a>
+                            </li>
+                        </ul>
+                    </div>
+                </li>
+
+                <li>
+                    <a href="#category" data-bs-toggle="collapse">
+                        <i class="mdi mdi-email-multiple-outline"></i>
+                        <span> Category </span>
+                        <span class="menu-arrow"></span>
+                    </a>
+                    <div class="collapse" id="category">
+                        <ul class="nav-second-level">
+                            <li>
+                                <a href="{{ route('all.category') }}">All Category </a>
+                            </li>
+
+                        </ul>
+                    </div>
+                </li>
+
+                <li>
+                    <a href="#product" data-bs-toggle="collapse">
+                        <i class="mdi mdi-email-multiple-outline"></i>
+                        <span> Products </span>
+                        <span class="menu-arrow"></span>
+                    </a>
+                    <div class="collapse" id="product">
+                        <ul class="nav-second-level">
+                            <li>
+                                <a href="{{ route('all.product') }}">All Product </a>
+                            </li>
+
+                            <li>
+                                <a href="{{ route('add.product') }}">Add Product </a>
+                            </li>
+                        </ul>
+                    </div>
+                </li>
+
+                <li>
+                    <a href="#orders" data-bs-toggle="collapse">
+                        <i class="mdi mdi-email-multiple-outline"></i>
+                        <span> Orders </span>
+                        <span class="menu-arrow"></span>
+                    </a>
+                    <div class="collapse" id="orders">
+                        <ul class="nav-second-level">
+                            <li>
+                                <a href="{{ route('pending.order') }}">Pending Orders </a>
+                            </li>
+                            <li>
+                                <a href="{{ route('complete.order') }}">Complete Orders </a>
+                            </li>
+                        </ul>
+                    </div>
+                </li>
+            </ul>
+        </div>
+        </li>
+        </ul>
+
+    </div>
+    <!-- End Sidebar -->
+
+    <div class="clearfix"></div>
+
+</div>
+<!-- Sidebar -left -->
+
+</div>
---
+++ routes/web.php
@@ -0,0 +1,139 @@
+<?php
+  
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\Backend\EmployeeController;
+use App\Http\Controllers\Backend\CustomerController;
+use App\Http\Controllers\Backend\SupplierController;
+use App\Http\Controllers\Backend\CategoryController;
+use App\Http\Controllers\Backend\ProductController;
+use App\Http\Controllers\Backend\OrderController;
+use App\Http\Controllers\Backend\RoleController;
+ 
+/* 
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('auth.login');
+});
+
+Route::get('/dashboard', function () {
+    return view('index');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
+
+
+Route::get('/admin/logout', [AdminController::class, 'AdminDestroy'])->name('admin.logout');
+
+Route::get('/logout', [AdminController::class, 'AdminLogoutPage'])->name('admin.logout.page');
+
+
+Route::middleware(['auth'])->group(function(){ 
+
+  Route::get('/admin/profile', [AdminController::class, 'AdminProfile'])->name('admin.profile');
+  Route::post('/admin/profile/store', [AdminController::class, 'AdminProfileStore'])->name('admin.profile.store');
+  Route::get('/change/password', [AdminController::class, 'ChangePassword'])->name('change.password');
+  Route::post('/update/password', [AdminController::class, 'UpdatePassword'])->name('update.password');
+
+
+  /// Employee All Route 
+  Route::controller(EmployeeController::class)->group(function(){
+
+  Route::get('/all/employee','AllEmployee')->name('all.employee');
+  Route::get('/add/employee','AddEmployee')->name('add.employee');
+  Route::post('/store/employee','StoreEmployee')->name('employee.store');
+  Route::get('/edit/employee/{id}','EditEmployee')->name('edit.employee');
+  Route::post('/update/employee','UpdateEmployee')->name('employee.update');
+  Route::get('/delete/employee/{id}','DeleteEmployee')->name('delete.employee');
+  });
+
+  /// Customer All Route 
+  Route::controller(CustomerController::class)->group(function(){
+
+  Route::get('/all/customer','AllCustomer')->name('all.customer');
+  Route::get('/add/customer','AddCustomer')->name('add.customer');
+  Route::post('/store/customer','StoreCustomer')->name('customer.store');
+  Route::get('/edit/customer/{id}','EditCustomer')->name('edit.customer');
+  Route::post('/update/customer','UpdateCustomer')->name('customer.update');
+  Route::get('/delete/customer/{id}','DeleteCustomer')->name('delete.customer');
+  });
+
+  /// Supplier All Route 
+  Route::controller(SupplierController::class)->group(function(){
+
+  Route::get('/all/supplier','AllSupplier')->name('all.supplier');
+  Route::get('/add/supplier','AddSupplier')->name('add.supplier');
+  Route::post('/store/supplier','StoreSupplier')->name('supplier.store');
+  Route::get('/edit/supplier/{id}','EditSupplier')->name('edit.supplier');
+  Route::post('/update/supplier','UpdateSupplier')->name('supplier.update');
+  Route::get('/delete/supplier/{id}','DeleteSupplier')->name('delete.supplier');
+  Route::get('/details/supplier/{id}','DetailsSupplier')->name('details.supplier');
+  });
+
+
+  ///Category All Route 
+  Route::controller(CategoryController::class)->group(function(){
+
+  Route::get('/all/category','AllCategory')->name('all.category');
+  Route::post('/store/category','StoreCategory')->name('category.store');  
+  Route::get('/edit/category/{id}','EditCategory')->name('edit.category');
+  Route::post('/update/category','UpdateCategory')->name('category.update'); 
+  Route::get('/delete/category/{id}','DeleteCategory')->name('delete.category'); 
+
+  });
+
+
+  ///Product All Route 
+  Route::controller(ProductController::class)->group(function(){
+
+  Route::get('/all/product','AllProduct')->name('all.product');
+  Route::get('/add/product','AddProduct')->name('add.product');
+  Route::post('/store/product','StoreProduct')->name('product.store');
+  Route::get('/edit/product/{id}','EditProduct')->name('edit.product');
+  Route::post('/update/product','UdateProduct')->name('product.update');
+  Route::get('/delete/product/{id}','DeleteProduct')->name('delete.product');
+
+  Route::get('/barcode/product/{id}','BarcodeProduct')->name('barcode.product');
+
+  Route::get('/import/product','ImportProduct')->name('import.product');
+  Route::get('/export','Export')->name('export');
+  Route::post('/import','Import')->name('import');
+  });
+
+  ///Order All Route 
+  Route::controller(OrderController::class)->group(function(){
+
+  Route::post('/final-invoice','FinalInvoice');
+  Route::get('/pending/order','PendingOrder')->name('pending.order');
+  Route::get('/order/details/{order_id}','OrderDetails')->name('order.details');
+  Route::post('/order/status/update','OrderStatusUpdate')->name('order.status.update');
+
+  Route::get('/complete/order','CompleteOrder')->name('complete.order');
+
+  Route::get('/stock','StockManage')->name('stock.manage');
+  Route::get('/order/invoice-download/{order_id}','OrderInvoice');
+
+  //// Due All Route 
+
+//   Route::get('/pending/due','PendingDue')->name('pending.due');
+//   Route::get('/order/due/{id}','OrderDueAjax');
+//   Route::post('/update/due','UpdateDue')->name('update.due');
+  });
+
+});
